fc11c085053066840b15c93c70fa374f57944025
mshahrokni <shahrokni.trivia@gmail.com>
2020-02-02 10:14:11 +0330

    commit the new file

---
+++ Date-Problems.js
@@ -0,0 +1,37 @@
+function addDays2Day(weekDay, days) {
+
+    const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    let weekDayIndex = 0;
+
+    //Find exact weekDay index
+
+    for (let i = 0; i < weekDays.length; i++) {
+
+        if (weekDays[i] === weekDay) {
+            weekDayIndex = i;
+            break;
+        }
+    }
+
+    if (weekDayIndex + days <= 6)
+        return weekDays[weekDayIndex + days];
+
+    let remainingDay = days % 7;
+
+    if (weekDayIndex + remainingDay <= 6)
+        return weekDays[weekDayIndex + remainingDay];
+    else
+        return weekDays[(weekDayIndex + remainingDay) - 7];
+}
+
+function getIsoDateDay(isoDate) {
+
+    // TODO: add REGEX in order to check the string date format
+    if (isoDate.length != 10)
+        return "incorrect format";
+
+    let date = new Date(isoDate);
+    let day = date.toString().substr(0, 3);
+
+    return day;
+}
